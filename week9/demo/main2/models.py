830ea2633b1d0ad792c0deadde33cd2334b3c2df
bobur554395 <bobur.muhsimbaev@gmail.com>
2018-10-22 10:30:26 +0600

    Week 10

---
+++ week9/demo/main2/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+
+
+class StudentManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(created_by=user)
+    
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    objects = StudentManager()
+
+    def __str__(self):
+        return "{}: {}".format(self.id, self.name)
+
+    def get_absolute_url(self):
+        return reverse_lazy('main2:student_list')
